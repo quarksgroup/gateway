83335825df21ad35d82a88ec9b81ea2de9c654f0
ganzagauzy <ganzantambuko@gmail.com>
2022-12-05 11:16:10 +0200

    new site

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+$(document).ready(function(){
+
+    // wow initiation
+    new WOW().init();
+
+    // navigation bar toggle
+    $('#navbar-toggler').click(function(){
+        $('.navbar-collapse').slideToggle(400);
+    });
+
+    // navbar bg change on scroll
+    $(window).scroll(function(){
+        let pos = $(window).scrollTop();
+        if(pos >= 100){
+            $('.navbar').addClass('cng-navbar');
+        } else {
+            $('.navbar').removeClass('cng-navbar');
+        }
+    });
+
+    // sample video popup
+    $(document).ready(function() {
+        $('.popup-youtube').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+    
+            fixedContentPos: false
+        });
+    });
+
+    
+
+  
+   
+
+});
